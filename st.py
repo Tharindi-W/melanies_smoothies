8b8fce980b0dda5c2589002e94f6a886f07bea9b
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 02:46:12 +0530

    Update st.py

--- st.py
+++ st.py
@@ -14,7 +14,8 @@ Name_on_order = st.text_input('Name on smoothie:')
 st.write('Name on your smoothie will be:',Name_on_order )
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')),col('SEARCH_ON')
+st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
