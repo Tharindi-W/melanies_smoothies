e9940f44f7573f502db9c60de345f48d891e459a
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 02:42:13 +0530

    Update st.py

--- st.py
+++ st.py
@@ -26,6 +26,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" )
+        fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,Name_on_order)
             values ('""" + ingredients_string + """','""" + Name_on_order + """')"""
@@ -37,6 +39,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered,{Name_on_order}!', icon="✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" )
-#st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
