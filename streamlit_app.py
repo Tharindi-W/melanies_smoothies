33d51b7dedf98ac4efda58969d9570a6702f1b33
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 03:28:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen+'Nutrition Information')
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_chosen)
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,Name_on_order)
