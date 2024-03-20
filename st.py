8621ce54a135f20a885a585d7e2bd81f6c47ef25
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 02:49:04 +0530

    Update st.py

--- st.py
+++ st.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
