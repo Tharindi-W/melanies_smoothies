af1a84d2b329e2317c015655b01119830a139e12
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 02:29:49 +0530

    Update st.py

--- st.py
+++ st.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -36,3 +36,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered,{Name_on_order}!', icon="✅")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" )
+st.text(fruityvice_response)
