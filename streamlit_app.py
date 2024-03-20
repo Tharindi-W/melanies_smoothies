831fcc88ac11826f51606635c682707f53a80ea1
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 00:01:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.write(
     """Choose the fruits you want in your custom smoothie!
     """)
 
-session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
