6ebe68b6c5c7dc4639e1bb3f0029d273fa93ed8a
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 01:37:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ pd_df= my_dataframe.to_pandas()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
+    , max_selections = 5
     )
 if ingredients_list:
     ingredients_string = ''
