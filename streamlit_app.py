6e2b86febcdab2da34aa7d089a52fcee25e86ef3
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 00:20:39 +0530

    Update streamlit_app.py
    added file type json

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
