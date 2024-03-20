9596c5390476cf1765bf2f6f47ef292998860940
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 02:40:27 +0530

    Update st.py

--- st.py
+++ st.py
@@ -39,4 +39,4 @@ if ingredients_list:
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" )
 #st.text(fruityvice_response.json())
-fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE)
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
