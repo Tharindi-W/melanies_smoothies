1cae543338376e89415a7a885463e757ce3c8717
Tharindi-W <142558857+Tharindi-W@users.noreply.github.com>
2024-03-21 01:19:36 +0530

    Update streamlit_app.py
    adding stop and checking dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
